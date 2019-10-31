5a4d613c28cc2026a4e4075bcfe52423efce7322
DSamodra <48814594+DSamodra@users.noreply.github.com>
2019-10-31 20:35:55 +0700

    SG Pertama

---
+++ SG_PROGRAMING_1.m
@@ -0,0 +1,41 @@
+disp("menghitung luas persegi");
+sisi = input('Masukkan panjang sisi : ');
+luas_persegi = sisi * 4;
+disp("Luas Persegi = " + luas_persegi);
+
+disp("menghitung luas persegi panjang");
+panjang = input('Masukkan panjang persegi : ');
+ls = input('Masukkan lebar persegi : ');
+luas_persegi_panjang = (2*panjang) + (2*ls);
+disp("Luas Persegi = " + luas_persegi_panjang);
+
+disp("menghitung luas jajar genjang");
+alas = input('Masukkan alas : ');
+tinggi = input('Masukkan tinggi : ');
+luas_jajar_genjang = alas * tinggi;
+disp("Luas Persegi = " + luas_persegi_panjang);
+
+disp("menghitung luas trapesium");
+sisi1= input('Masukkan sisi1 : ');
+sisi2 = input('Masukkan sisi2 : ');
+tinggi = input('Masukkan tinggi : ');
+luas_trapesium = ((sisi1 + sisi2)*tinggi)/2;
+disp("Luas Persegi = " + luas_trapesium);
+
+disp("menghitung luas belah ketupat");
+diameter1= input('Masukkan Diameter 1 : ');
+diameter2 = input('Masukkan Diameter 2 : ');
+luas_belahketupat = (diameter1 * diameter2)/2;
+disp("Luas Persegi = " + luas_trapesium);
+
+disp("menghitung luas segitiga");
+al= input('Masukkan alas : ');
+ting = input('Masukkan tinggi : ');
+luas_segitiga = (al * ting)/2;
+disp("Luas Persegi = " + luas_trapesium);
+
+disp("menghitung luas lingkaran");
+jari = input('Masukkan jari-jari : ');
+phi = 3.14;
+luas_lingkaran = phi * jari * jaari;
+disp("Luas Persegi = " + luas_lingkaran);
